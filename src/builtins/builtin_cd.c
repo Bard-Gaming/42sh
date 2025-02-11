a7f68e88a0df2e7e4cce08f93eb18b29accbf302
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-11 23:05:26 +0100

    [src/builtins] Updated cd builtin to switch to user's home directory if the given path is "~"

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -12,6 +12,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 static const char *get_home_dir(char *env[])
@@ -24,6 +25,13 @@ static const char *get_home_dir(char *env[])
     return "";
 }
 
+static bool is_home_alias(const char *value)
+{
+    if (value == NULL)
+        return true;
+    return value[0] == '~' && value[1] == '\0';
+}
+
 int builtin_cd(const char *args[], char *env[])
 {
     struct stat stat_buf;
@@ -33,7 +41,7 @@ int builtin_cd(const char *args[], char *env[])
         sh_puterr("cd: Too many arguments.\n");
         return 84;
     }
-    path = args[1] == NULL ? get_home_dir(env) : args[1];
+    path = is_home_alias(args[1]) ? get_home_dir(env) : args[1];
     if (stat(path, &stat_buf) != 0) {
         sh_puterr(path);
         sh_puterr(": No such file or directory.\n");
