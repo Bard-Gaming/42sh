5cced21c57c5319d0c6effd9d0a47a396e1e7583
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-11 17:31:11 +0100

    [src/shell] Fixed shell displaying prompt in tty mode

--- src/shell/shell_mainloop.c
+++ src/shell/shell_mainloop.c
@@ -9,13 +9,15 @@
 #include <mysh/shell.h>
 #include <mysh/arguments.h>
 #include <mysh/io.h>
+#include <unistd.h>
 #include <stdio.h>
 
 
 static ssize_t query_command(char **restrict lineptr,
     size_t *restrict n, FILE *restrict stream)
 {
-    sh_putstr("$> ");
+    if (isatty(0))
+        sh_putstr("$> ");
     return getline(lineptr, n, stream);
 }
 
