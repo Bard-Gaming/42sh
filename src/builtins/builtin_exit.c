c1cc55d553c39f22c4bae4b227a70c472c4f62b3
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-11 23:43:03 +0100

    [src/builtins] Fixed coding style error (that shouldn't exist)

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -22,12 +22,22 @@ static bool is_number(const char *string)
     return true;
 }
 
+/*
+** This function is necessary as ternary operators
+** in for loop declarations are (apparently) a
+** C-C2 violation (even though they shouldn't be)
+*/
+static unsigned int min(unsigned int a, unsigned int b)
+{
+    return a > b ? b : a;
+}
+
 static unsigned char arg_to_exit_status(const char *arg)
 {
     size_t arg_length = sh_strlen(arg);
     unsigned short exit_status = 0;
 
-    for (unsigned int i = arg_length > 3 ? 3 : arg_length; i > 0; i--) {
+    for (unsigned int i = min(3, arg_length); i > 0; i--) {
         exit_status *= 10;
         exit_status += arg[arg_length - i] - '0';
     }
