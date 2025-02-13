f3f527b0003f6244e076b4662dda4535072a97de
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-13 22:44:32 +0100

    [src/builtins] Updated 'cd' builtin to allow going to previous dir (OLDPWD)

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -14,13 +14,6 @@
 #include <stdbool.h>
 
 
-static const char *get_home_dir(sh_env_t *env)
-{
-    const char *dir = sh_env_get(env, "HOME");
-
-    return dir == NULL ? "/" : dir;
-}
-
 static bool is_home_alias(const char *value)
 {
     if (value == NULL)
@@ -28,19 +21,39 @@ static bool is_home_alias(const char *value)
     return value[0] == '~' && value[1] == '\0';
 }
 
+static const char *parse_dir_path(const char *raw_path, sh_env_t *env)
+{
+    const char *dir;
+
+    if (is_home_alias(raw_path)) {
+        dir = sh_env_get(env, "HOME");
+        return dir == NULL ? "/" : dir;
+    }
+    if (raw_path[0] == '-' && raw_path[1] == '\0') {
+        dir = sh_env_get(env, "OLDPWD");
+        return dir == NULL ? "" : dir;
+    }
+    return raw_path;
+}
+
 int builtin_cd(const char *args[], sh_env_t *env)
 {
     const char *path;
+    char *old_path;
 
     if (args[1] != NULL && args[2] != NULL) {
         sh_puterr("cd: Too many arguments.\n");
         return 84;
     }
-    path = is_home_alias(args[1]) ? get_home_dir(env) : args[1];
+    path = parse_dir_path(args[1], env);
+    old_path = getcwd(NULL, 0);
     if (chdir(path) != 0) {
         sh_puterr(path);
         sh_puterr(": No such file or directory.\n");
+        free(old_path);
         return 84;
     }
+    sh_env_set(env, "OLDPWD", old_path);
+    free(old_path);
     return 0;
 }
