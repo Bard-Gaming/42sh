39cb969974b58cd70ce481ea2ed3c3857cb5c04b
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-14 11:43:24 +0100

    [src/shell] Updated shell to handle a failed fork

--- src/shell/shell_subprocess.c
+++ src/shell/shell_subprocess.c
@@ -14,6 +14,12 @@
 #include <stdlib.h>
 
 
+pid_t fork_error(void)
+{
+    sh_puterr("mysh: critical error: failed to fork\n");
+    return -1;
+}
+
 /*
 ** Create a subprocess with given args
 ** and get its process id.
@@ -23,6 +29,8 @@ pid_t shell_subprocess(const char *program, char **args, char *env[])
 {
     pid_t subproc = fork();
 
+    if (subproc == -1)
+        return fork_error();
     if (subproc != 0)
         return subproc;
     execve(program, args, env);
