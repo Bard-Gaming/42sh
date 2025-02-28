72fb4152aed746bf85307ab77510385b5f1fe967
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-28 11:52:40 +0100

    [src/env] Fixed _set function breaking after recent sh_strcpy changes

--- src/env/sh_env_set.c
+++ src/env/sh_env_set.c
@@ -16,9 +16,9 @@ void sh_env_set(sh_env_t *env, const char *name, const char *value)
     size_t value_len = sh_strlen(value);
     char *item_value = malloc((name_len + value_len + 2) * sizeof(char));
 
-    item_value[name_len] = '=';
     sh_strcpy(item_value, name);
     sh_strcpy(item_value + name_len + 1, value);
+    item_value[name_len] = '=';
     item_value[name_len + value_len + 1] = '\0';
     sh_env_put(env, item_value);
     free(item_value);
