de443e85592180100efa43ee989d42b794bc4c1f
Bard-Gaming <christophe.dronne@epitech.eu>
2025-03-28 19:39:33 +0100

    [lib/parse] Updated parsing library

--- lib/42parser/src/parser/parse_pipeline.c
+++ lib/42parser/src/parser/parse_pipeline.c
@@ -7,18 +7,25 @@
 */
 
 #include <42parser/parser.h>
+#include <42parser/error.h>
 
 
 ast_t *parse_pipeline(parser_t *parser, ast_t *atom)
 {
     ast_t *result = ast_create(AT_PIPELINE);
     ast_program_t *nodes = ast_program_create();
+    ast_t *current;
 
     result->data = nodes;
     ast_program_append(nodes, atom);
     while (parser->current->type == TT_PIPE) {
         parser_next(parser);
-        ast_program_append(nodes, parse_atom(parser));
+        current = parse_atom(parser);
+        ast_program_append(nodes, current);
+        if (current->type == AT_ERROR) {
+            parser_errno_set(PE_NULL_COMMAND);
+            return result;
+        }
     }
     return result;
 }
